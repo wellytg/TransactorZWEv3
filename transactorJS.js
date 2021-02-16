9fa11dff40ba1defa2284eb6e31f427305141c40
Welly T Gee <wellytg@gmail.com>
2021-02-16 20:39:29 +0200

    made edit to pos left only fixed rates

--- transactorJS.js
+++ transactorJS.js
@@ -3,19 +3,14 @@ function calc(amount) {
   console.log(amount);
   var amount = Number(amount);
   var imtGvt = Number((amount * 0.02).toFixed(2));
-  var imtGvtPOS = imtGvt;
-
-  // POS/Swipe variables
-  var posCharges = 10;
-  var posIMT = 0;
-  var posIMTminbal = 0;
+  var BankCharges = Number((amount * 0.03).toFixed(2));
 
   // Error Message variables
   var errMsgPOS = document.getElementById("errMsgPOS");
   var errMsgBank = document.getElementById("errMsgBank");
 
   // Bank charge variables
-  var transferChg = 25;
+
   var bankIMT = 0;
   var minBalTransfer = 0;
 
@@ -31,56 +26,19 @@ function calc(amount) {
   // Calculate the Charges of the entered Amount
   switch (true) {
     case amount == 0:
-      posCharges = 0;
-      posIMT = 0;
-      posIMTminbal = 0;
-
-      transferChg = 0;
       bankIMT = 0;
       minBalTransfer = 0;
 
       break;
 
-    case amount > 0.99 && amount < 667:
-      posCharges = 10;
-      posIMT = posCharges + imtGvt;
-      posIMTminbal = amount + posIMT;
-
-      transferChg = 25;
-      bankIMT = Number(transferChg + imtGvt);
-      minBalTransfer = Number(amount + bankIMT);
-
-      break;
-
-    case amount >= 667 && amount < 1334:
-      posCharges = amount * 0.015;
-      posIMT = Number(posCharges + imtGvt);
-      posIMTminbal = Number(amount + posIMT);
-
-      transferChg = 25;
-      bankIMT = Number(transferChg + imtGvt);
-      minBalTransfer = Number(amount + bankIMT);
-      break;
-
-    case amount >= 1334 && amount < 20000.01:
-      posCharges = 20;
-      posIMT = posCharges + imtGvt;
-      posIMTminbal = amount + posIMT;
-
-      transferChg = 25;
-      bankIMT = Number(transferChg + imtGvt);
+    case amount >= 0.99 && amount < 20000.01:
+      bankIMT = Number(BankCharges + imtGvt);
       minBalTransfer = Number(amount + bankIMT);
 
       break;
 
     case amount > 20000 && amount < 500000.01:
-      imtGvtPOS = 0;
-      posCharges = 0;
-      posIMT = 0;
-      posIMTminbal = 0;
-
-      transferChg = 25;
-      bankIMT = Number(transferChg + imtGvt);
+      bankIMT = Number(BankCharges + imtGvt);
       minBalTransfer = Number(amount + bankIMT);
 
       errMsgPOS.style.display = "block";
@@ -89,13 +47,8 @@ function calc(amount) {
       break;
 
     case amount > 500000:
-      imtGvtPOS = 0;
       imtGvt = 0;
-      posCharges = 0;
-      posIMT = 0;
-      posIMTminbal = 0;
-
-      transferChg = 0;
+      BankCharges = 0;
       bankIMT = 0;
       minBalTransfer = 0;
 
@@ -109,38 +62,22 @@ function calc(amount) {
 
   updateElements(
     imtGvt,
-    Number(posCharges.toFixed(2)),
-    Number(posIMT.toFixed(2)),
-    Number(posIMTminbal.toFixed(2)),
-    imtGvtPOS,
-    Number(transferChg.toFixed(2)),
     Number(bankIMT.toFixed(2)),
-    Number(minBalTransfer.toFixed(2))
+    Number(minBalTransfer.toFixed(2)),
+    BankCharges
   );
 }
 
-function updateElements(
-  imtGvt,
-  posCharges,
-  posIMT,
-  posIMTminbal,
-  imtGvtPOS,
-  transferChg,
-  bankIMT,
-  minBalTransfer
-) {
+function updateElements(imtGvt, bankIMT, minBalTransfer, BankCharges) {
   // IMT Gvt 2% tax update
   document.getElementById("imtGvt").value = imtGvt;
 
-  //for POS/Swipe updates
-  document.getElementById("imtGvtp").value = imtGvtPOS;
-  document.getElementById("posCharges").value = posCharges;
-  document.getElementById("posIMT").value = posIMT;
-  document.getElementById("posIMTminbal").value = posIMTminbal;
+  //BankChargesb
+  document.getElementById("BankCharges").value = BankCharges;
 
   //for Bank Transfer updates
   document.getElementById("imtGvtb").value = imtGvt;
-  document.getElementById("transferCharges").value = transferChg;
+  document.getElementById("BankChargesb").value = BankCharges;
   document.getElementById("bankIMT").value = bankIMT;
   document.getElementById("minBalTransfer").value = minBalTransfer;
 
